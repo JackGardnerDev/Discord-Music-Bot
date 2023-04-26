5e1143a4d984f7cd3ffea7fccf33d929bde2b2db
Jack Gardner <jackmcggardner@outlook.com>
2023-04-27 00:09:19 +1000

    Init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#main.py
+
+import discord
+from discord.ext import commands
+import music
+
+cogs = [music]
+
+client = commands.Bot(command_prefix='?', intents = discord.Intents.all())
+
+for i in range(len(cogs)):
+    cogs[i].setup(client)
+
+#Insert token here
+client.run("")
